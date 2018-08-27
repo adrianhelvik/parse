1ed790e833b22064ccfd9897ff9500f5ad64bc97
Adrian Helvik <adrian@adrian.local>
2018-08-27 13:37:30 +0200

    Disabled test

--- pascal/primes.spec.js
+++ pascal/primes.spec.js
@@ -6,7 +6,7 @@ import fs from 'fs'
 
 const source = fs.readFileSync(__dirname + '/primes.pas', 'utf-8')
 
-it('can parse the source', () => {
+xit('can parse the source', () => {
   const tokens = lex({ source, syntax })
   // const rule = convertSyntax(syntax)
   const ast = parse({ tokens, source, syntax })
